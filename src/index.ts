a834019a9a386b90b3e8eb9e706798bccf496d9b
briankopp <briankopp.usa@gmail.com>
2019-09-29 13:43:45 -0600

    Reorganizing main file to put the most important stuff at the top

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,74 @@ import { PrometheusExpressOptions } from './prometheus-express-options';
 
 const debug = debugLibrary('PROMEXPRESS');
 
+export const promExpressMiddleware = (opts?: PrometheusExpressOptions) => {
+    debug('setting up prometheus-express middleware');
+    // configure middleware
+    const options: PrometheusExpressOptions = defaultOptions;
+
+    const routeDevaluer = new UrlDevaluer();
+    if (options.collectDefaultPrometheusMetrics) {
+        debug('starting prometheus default metric collection');
+        collectDefaultMetrics(options.prometheusDefaultCollectorOptions);
+    }
+
+    let counter: Counter = null;
+    if (options.collectRequestCounts) {
+        counter = new Counter({
+            name: options.requestCounterMetricName,
+            help: 'Count of http requests, organized by method and route',
+            labelNames: ['method', 'route']
+        });
+    }
+    let timings: Histogram = null;
+    if (options.collectRequestTimings) {
+        timings = new Histogram({
+            name: options.requestTimingMetricName,
+            help: 'Response time of request, in milliseconds, organized by method, route, and status code group',
+            labelNames: ['method', 'route', 'code'],
+            buckets: options.timingHistogramBuckets
+        });
+    }
+
+    // return the middleware
+    return (req: Request, res: Response, next: NextFunction): void => {
+        const route = routeDevaluer.devalueUrl(req.path);
+        const method = req.method;
+
+        debug(`received request: ${method} ${route}`);
+
+        if (options.collectRequestCounts) {
+            debug('incrementing request');
+            counter.labels(method, route).inc();
+        }
+
+        if (options.collectRequestTimings) {
+            const start = Date.now();
+            res.on('finish', () => {
+                const end = Date.now();
+                // give 1 decimal precision in milliseconds
+                const responseTime = Math.round(10.0 * (end - start)) / 10.0;
+                const groupedCode = groupStatusCode(res.statusCode);
+                debug(
+                    `recording ${method} ${route} request with status code
+                    ${res.statusCode} in status code group ${groupedCode}
+                    with time ${responseTime}`
+                );
+                timings.labels(method, route, groupedCode).observe(responseTime);
+            });
+        }
+
+        if (options.exposeMetrics && route === options.metricsRoute) {
+            debug('metrics endpoint hit, returning prometheus metrics');
+            res.set('Content-Type', register.contentType).send(register.metrics());
+            return;
+        }
+
+        // pass along to next middleware/app
+        next();
+    };
+};
+
 const defaultOptions: PrometheusExpressOptions = {
     exposeMetrics: true,
     metricsRoute: '/metrics',
@@ -18,28 +86,8 @@ const defaultOptions: PrometheusExpressOptions = {
     prometheusDefaultCollectorOptions: null
 };
 
-const groupStatusCode = (code: number): string => {
-    if (code < 100) {
-        return 'ERR';
-    } else if (code < 200) {
-        return '1XX';
-    } else if (code < 300) {
-        return '2XX';
-    } else if (code < 400) {
-        return '3XX';
-    } else if (code < 500) {
-        return '4XX';
-    } else if (code < 600) {
-        return '5XX';
-    } else {
-        return 'ERR';
-    }
-};
-
-export const promExpressMiddleware = (opts?: PrometheusExpressOptions) => {
-    debug('setting up prometheus-express middleware');
-    // configure middleware
-    const options: PrometheusExpressOptions = defaultOptions;
+const parseOptions = (opts: PrometheusExpressOptions): PrometheusExpressOptions => {
+    const options = defaultOptions;
     if (opts) {
         debug('parsing passed options');
         if (
@@ -108,62 +156,23 @@ export const promExpressMiddleware = (opts?: PrometheusExpressOptions) => {
         }
     }
 
-    const routeDevaluer = new UrlDevaluer();
-    if (options.collectDefaultPrometheusMetrics) {
-        debug('starting prometheus default metric collection');
-        collectDefaultMetrics(options.prometheusDefaultCollectorOptions);
+    return options;
+};
+
+const groupStatusCode = (code: number): string => {
+    if (code < 100) {
+        return 'ERR';
+    } else if (code < 200) {
+        return '1XX';
+    } else if (code < 300) {
+        return '2XX';
+    } else if (code < 400) {
+        return '3XX';
+    } else if (code < 500) {
+        return '4XX';
+    } else if (code < 600) {
+        return '5XX';
+    } else {
+        return 'ERR';
     }
-
-    let counter: Counter = null;
-    if (options.collectRequestCounts) {
-        counter = new Counter({
-            name: options.requestCounterMetricName,
-            help: 'Count of http requests, organized by method and route',
-            labelNames: ['method', 'route']
-        });
-    }
-    let timings: Histogram = null;
-    if (options.collectRequestTimings) {
-        timings = new Histogram({
-            name: options.requestTimingMetricName,
-            help: 'Response time of request, in milliseconds, organized by method, route, and status code group',
-            labelNames: ['method', 'route', 'code'],
-            buckets: options.timingHistogramBuckets
-        });
-    }
-
-    // return the middleware
-    return (req: Request, res: Response, next: NextFunction): void => {
-        const route = routeDevaluer.devalueUrl(req.path);
-        const method = req.method;
-
-        debug(`received request: ${method} ${route}`);
-
-        if (options.collectRequestCounts) {
-            debug('incrementing request');
-            counter.labels(method, route).inc();
-        }
-
-        if (options.collectRequestTimings) {
-            const start = Date.now();
-            res.on('finish', () => {
-                const end = Date.now();
-                // give 1 decimal precision in milliseconds
-                const responseTime = Math.round(10.0 * (end - start)) / 10.0;
-                const groupedCode = groupStatusCode(res.statusCode);
-                debug(
-                    `recording ${method} ${route} request with status code
-                    ${res.statusCode} in status code group ${groupedCode}
-                    with time ${responseTime}`
-                );
-                timings.labels(method, route, groupedCode).observe(responseTime);
-            });
-        }
-
-        if (options.exposeMetrics && route === options.metricsRoute) {
-            debug('metrics endpoint hit, returning prometheus metrics');
-            res.set('Content-Type', register.contentType).send(register.metrics());
-            return;
-        }
-    };
 };
